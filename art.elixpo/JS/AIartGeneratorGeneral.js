f9d034e6f76ce07e463eff89ab78f3518297e0ad
Circuit-Overtime <ayushbhatt633@gmail.com>
2024-09-28 23:52:11 +0530

    Refactor server_networks/getImage.js to handle request queue and fallback images

--- art.elixpo/JS/AIartGeneratorGeneral.js
+++ art.elixpo/JS/AIartGeneratorGeneral.js
@@ -23,6 +23,7 @@ const firebaseConfig = {
   let tagUrl = '';
   let ai_enhanced_prompt = '';
   let suffixPrompt = "";
+  let serverReturnStatus = true;
 //new commit
 window.onload = function() {
     document.querySelector(".patternContainer").classList.remove("hidden");
@@ -183,7 +184,7 @@ let controller;
                     }
                     console.log(downloadUrl);
                     try {
-                        
+                        serverReturnStatus = true;
                         const response = await fetch(`${downloadUrl}/download-image`, {  //get image
                             method: 'POST',
                             headers: {
@@ -195,6 +196,11 @@ let controller;
         
                         if (!response.ok) {
                             throw new Error(`HTTP error! Status: ${response.status}`);
+                        }
+                        else if(response.status == 202)
+                        {
+                            serverReturnStatus = false;
+                            document.getElementById("acceptBtn").classList.add("hidden");
                         }
         
                         const data = await response.json();
@@ -297,7 +303,7 @@ async function generateMultipleImages(encodedPrompt, width, height, seeds, aspec
         generating = false;
         // tokenDeduct(aspectRatio, theme, numberOfImages);
         document.getElementById("rejectBtn").classList.remove("hidden");
-        if (document.getElementById("privateSwitch").checked == false) {
+        if ((document.getElementById("privateSwitch").checked == false) || !serverReturnStatus) {
             document.getElementById("acceptBtn").classList.add("hidden");
             document.getElementById("NotifTxt").innerText = "Greetings - (Private Result)";
             setTimeout(() => {
@@ -306,7 +312,17 @@ async function generateMultipleImages(encodedPrompt, width, height, seeds, aspec
             document.getElementById("enhancementAI").classList.add("hidden");
             document.getElementById("stopGeneration").classList.add("hidden");
             document.getElementById("NotifTxt").innerText = "Greetings!";
-        } else {
+        } 
+        else if ((document.getElementById("privateSwitch").checked == true) && serverReturnStatus) {
+            document.getElementById("acceptBtn").classList.remove("hidden");
+            document.getElementById("NotifTxt").innerText = "Greetings";
+            setTimeout(() => {
+                document.getElementById("savedMsg").classList.remove("display");
+            }, 1500);
+            document.getElementById("enhancementAI").classList.add("hidden");
+            document.getElementById("stopGeneration").classList.add("hidden");
+        } 
+        else{
             document.getElementById("acceptBtn").classList.remove("hidden");
             document.getElementById("NotifTxt").innerText = "Greetings";
             setTimeout(() => {
