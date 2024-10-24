c9b698efe11efb0a7033bf1a045119e396aee4ff
Circuit-Overtime <ayushbhatt633@gmail.com>
2024-10-24 19:02:17 +0530

    made the changes to the server.sh

--- server.sh
+++ server.sh
@@ -2,4 +2,6 @@
 
 # Start the Node.js servers in the background
 node /home/pi/Desktop/Elixpo_ai_pollinations/server_networks/getImage.js &
-echo "image.js is running in the background with PID $!"
+echo "image.js is running in the background with PID $!" & 
+node /home/pi/Desktop/Elixpo_ai_pollinations/discord_bot/elixpo_discord_bot.js &
+echo "elixpo_discord_bot.js is running in the background with PID $!" & 
