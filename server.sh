c56b9ea92f0c7f4a196ca5b501e6dfd6af373ff9
Circuit-Overtime <ayushbhatt633@gmail.com>
2024-10-24 22:34:06 +0530

    added fixes to directory systems

--- server.sh
+++ server.sh
@@ -2,7 +2,7 @@
 
 # Start the Node.js servers in the background
 node /home/pi/Desktop/Elixpo_ai_pollinations/server_networks/getImage.js &
-echo "image.js is running in the background with PID $!" & 
-cd /home/pi/Desktop/Elixpo_ai_pollinations/discord_bot &
+echo "image.js is running in the background with PID $!" 
+cd /home/pi/Desktop/Elixpo_ai_pollinations/discord_bot 
 node elixpo_discord_bot.js &
-echo "elixpo_discord_bot.js is running in the background with PID $!" & 
+echo "elixpo_discord_bot.js is running in the background with PID $!" 
