9ec522f1a2923cba112bc0a221327d0fee72ea8e
Circuit-Overtime <ayushbhatt633@gmail.com>
2025-06-05 10:55:55 +0530

    added the new project list to pollinations

--- art.elixpo/minecraft_mod_python/modBuild/src/main/java/com/example/Elixpo_image.java
+++ art.elixpo/minecraft_mod_python/modBuild/src/main/java/com/example/Elixpo_image.java
@@ -1,24 +1,28 @@
 package com.example;
 
 import net.fabricmc.api.ModInitializer;
-
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Elixpo_image implements ModInitializer {
 	public static final String MOD_ID = "elixpo_image";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		LOGGER.info("Elixpo mod initialized!");
 
-		LOGGER.info("Hello Fabric world!");
+		// Register the /elixpo command
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
+			dispatcher.register(CommandManager.literal("elixpo").executes(context -> {
+				ServerCommandSource source = context.getSource();
+				source.sendMessage(Text.literal("hello budy")); // Sends message to the command issuer
+				return 1; // Return success
+			}));
+		});
 	}
 }
