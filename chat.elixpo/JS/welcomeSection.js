e6d61f75599c18c41d6a0ff2ae86f4db48c7cde1
Circuit-Overtime <ayushbhatt633@gmail.com>
2025-06-09 14:58:38 +0530

    added the code for the news section + responsive

--- chat.elixpo/JS/welcomeSection.js
+++ chat.elixpo/JS/welcomeSection.js
@@ -1,1 +1,11 @@
 document.querySelector(".welcomeSection").scroll(0, 1126.4000244140625 )
+const seekBars = document.querySelectorAll('.newsplayBackSeek');
+
+seekBars.forEach((bar) => {
+    bar.addEventListener('click', () => {
+        // Remove .selected from all
+        seekBars.forEach(b => b.classList.remove('selected'));
+        // Add to the clicked one
+        bar.classList.add('selected');
+    });
+});
