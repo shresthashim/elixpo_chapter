7b9f438858d149b74ee14a72644ee56220cd17b6
Circuit-Overtime <ayushbhatt633@gmail.com>
2025-06-11 01:25:29 +0530

    put forward the news section js

---
+++ chat.elixpo/Public/JS/dailyNewsGeneral.js
@@ -0,0 +1,10 @@
+const seekBars = document.querySelectorAll('.newsplayBackSeek');
+
+seekBars.forEach((bar) => {
+    bar.addEventListener('click', () => {
+        // Remove .selected from all
+        seekBars.forEach(b => b.classList.remove('selected'));
+        // Add to the clicked one
+        bar.classList.add('selected');
+    });
+});
