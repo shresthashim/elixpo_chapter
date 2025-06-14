0c6c643c6cd2b17d02f511addf358966738f567e
Circuit-Overtime <ayushbhatt633@gmail.com>
2025-06-15 02:44:15 +0530

    updated vercrel style deployment

--- chat.elixpo/server.js
+++ chat.elixpo/server.js
@@ -115,8 +115,10 @@ app.use((req, res) => {
   res.status(404).sendFile(path.join(__dirname, 'public', 'oopsie.html'));
 });
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+if (!process.env.VERCEL) {
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });
+}
 
 export default app;
