87df6a5e06bf57754efa7163d6279e27f3a9c55b
Circuit-Overtime <ayushbhatt633@gmail.com>
2025-07-12 10:38:31 +0530

    regulated the section for recommendations + added new recommendation

--- elixpo/JS/recommendations.js
+++ elixpo/JS/recommendations.js
@@ -4,6 +4,12 @@ let recommendations = [
     "name" : "Dr. Subarsha Banerjee",
     "designation" : "Assistant Professor in JIS University",
     "image": "https://media.licdn.com/dms/image/v2/C4E03AQGIk84i2XqYkQ/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1649254631143?e=1757548800&v=beta&t=DiKOLLNyz-Q-qRxG9v-Nn9njWcXVI9n1U2YHcFsxFN0"
+    },
+    {
+    "content" : "As a student, Ayushman has consistently demonstrated a deep curiosity and dedication towards learning. His academic performance has been marked by a strong grasp of complex concepts and a remarkable ability to apply theoretical knowledge in practical situations. Whether it was through class participation, project work, or independent research, Ayushman has shown a commitment to excellence that sets him apart.",
+    "name" : "Dr. Bidisha Bhabani",
+    "designation" : "Assistant Professor in JIS University",
+    "image": "https://media.licdn.com/dms/image/v2/D5603AQFVTKreK1ABIQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1705933823066?e=1757548800&v=beta&t=jFjbixOgOp3mHwFuXO2DwsfrKjCiUWhHcWdULp2PqzA"
     }
 ]
 
@@ -16,7 +22,7 @@ function appendRecommendation(recommendations)
         let recommendationTile = `
         <div id="scrollZone" class="recommendationCard shrink-0 relative h-[400px] w-[650px] border-4 border-[#222] rounded-[25px] bg-[#E2D9C8] p-5 justify-center items-center mr-5 cursor-pointer hover:brightness-[85%] transition-all duration-300 ease-in-out">
           <div class="dottedContainer relative h-full w-full border-2 border-dashed border-[#222] rounded-[30px] flex flex-col gap-5 p-10 justify-between">
-            <p class="quote text-[#333] text-[1.55em] whitespace-normal break-words underline decoration-1 decoration-[0.05rem] decoration-[#888] underline-offset-[6px]"> "${recommendation.content}"</p>
+            <p class="quote text-[#333] text-[1.55em] whitespace-normal break-words underline decoration-1 decoration-[0.05rem] decoration-[#888] underline-offset-[6px]"> "${(recommendation.content).slice(0,200)}..."</p>
             <div class="attribution flex flex-row items-center items-left gap-5">
               <div class="userLogo relative bg-[url(${recommendation.image})] bg-cover bg-center h-[80px] w-[80px] border-2 border-[#222] rounded-[50%] grayscale sepia-[50%]"></div>
               <div class="userInfo flex flex-col gap-1 leading-[30px]">
