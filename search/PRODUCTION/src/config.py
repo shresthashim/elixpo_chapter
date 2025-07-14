9c0ad82bf9a548a8847690bff125659afb33ae51
Ayushman Bhattacharya <74301576+Circuit-Overtime@users.noreply.github.com>
2025-07-14 16:24:47 +0530

    Added new UI + Total Overhaul of the Searching Model (#14)
    * initiial addon to the modular code of elixpo-search
    
    * added the modular script
    
    * the plan script has been added
    
    * added all the modular files and the new code for the latest changes
    
    * added the new app entry point for production
    
    * added the code for the sse and event sourcing in this + fixed the youtube meta data bug
    
    * improved the connection to sse feed and subscribe to emits
    
    * GET, POST, V1/CHATCOMPLETIONS working, SSE not working, trying a fix
    
    * updated the code to work on SSE + GET + POST + V1/COMP
    
    * added the new systematic GET response for the new GET endpoint
    
    * fixed the issue of the SSE and fixed the issue of incomplete responses to client and added better UI
    
    * allowd full 5000 limit chubnks to be sent to the client side for SSE
    
    * Added the limitter to the app file getting ready for the final export
    
    * Added a config file containing all the config variables
    
    * switched from flask to uvicorn

---
+++ search/PRODUCTION/src/config.py
@@ -0,0 +1,7 @@
+get_youtube_video_metadata_show_log = False
+MAX_TRANSCRIPT_WORD_COUNT = 3000
+MAX_TOTAL_SCRAPE_WORD_COUNT = 1000
+MAX_IMAGES_TO_INCLUDE = 3
+scrape_website_show_log = True
+SCRAPE_IMAGE=True
+MAX_LINKS_TO_TAKE = 4
