76eef506f7ceb1e4f43f81b066860e33582d3147
IgYaHiko <subhrokolay2@gmail.com>
2025-07-15 16:31:42 +0530

    04:background-jobs

---
+++ src/inngest/functions.ts
@@ -0,0 +1,16 @@
+import { inngest } from "./client";
+
+export const helloWorld = inngest.createFunction(
+  { id: "hello-world" },
+  { event: "test/hello.world" },
+  async ({ event, step }) => {
+   
+    //imagine this is a transcripts step
+     await step.sleep("wait-a-moment", "10s");
+    
+
+    //imagine this is a summary step
+     await step.sleep("wait-a-moment", "5s");
+    return { message: `Hello ${event.data.email}!` };
+  },
+);
