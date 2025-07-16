c30abe37e6acbf2eba7bb88879ea9a5612bc3274
Circuit-Overtime <ayushbhatt633@gmail.com>
2025-07-16 23:24:41 +0530

    updated the code to have the new content return type

--- search/search_test_deploy.py
+++ search/search_test_deploy.py
@@ -1,7 +1,7 @@
 import requests
 import json
 import urllib.parse
-url = "https://search.pollinations.ai/search"
+url = "http://localhost:5000/search"
 headers = {"Content-Type": "application/json"}
 data = {
     "messages": [
