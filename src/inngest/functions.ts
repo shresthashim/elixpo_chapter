5a8044ea422c1dc95326799c4df03088653985a9
IgYaHiko <subhrokolay2@gmail.com>
2025-07-17 01:34:18 +0530

    05:AI-jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,16 +1,20 @@
 import { inngest } from "./client";
-
+import {  openai, createAgent } from "@inngest/agent-kit";
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
+  async ({ event }) => {
    
-    //imagine this is a transcripts step
-     await step.sleep("wait-a-moment", "10s");
-    
+     // Create a new agent with a system prompt (you can add optional tools, too)
+    const codeAgent = createAgent({
+      name: "codeAgent",
+      system: "You are CodeAgent. Write clean, simple, and readable code. Be concise. Avoid unnecessary abstractions. Default to JavaScript, TypeScript, or Python. Add brief inline comments only if needed. Follow best practices.",
+      model: openai({ model: "gpt-4o" }),
+    });
+    const {output} = await codeAgent.run(
+      `Sir here is your code: ${event.data.value}`
+    )
 
-    //imagine this is a summary step
-     await step.sleep("wait-a-moment", "5s");
-    return { message: `Hello ${event.data.email}!` };
+    return { output };
   },
 );
