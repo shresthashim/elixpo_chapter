ea9be226b271c4eaeebb36f446ed70219b62bfdc
Circuit-Overtime <ayushbhatt633@gmail.com>
2025-08-03 01:06:36 +0530

    updated to a safer approach towards deployment

--- chat.elixpo/BackendNode/firebaseConfig.js
+++ chat.elixpo/BackendNode/firebaseConfig.js
@@ -1,12 +1,16 @@
 import { initializeApp } from "firebase/app";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBUEuy0nkv9hOkkH-BTJAi6B9cQxTDmq0s",
+  apiKey: process.env.db_api,
   authDomain: "notes-89337.firebaseapp.com",
   databaseURL: "https://notes-89337-default-rtdb.firebaseio.com",
   projectId: "notes-89337",
   storageBucket: "notes-89337.appspot.com",
-  messagingSenderId: "739166822032",
-  appId: "1:739166822032:web:adafa65bf508eb86460215"
+  messagingSenderId: process.env.db_messaging_id,
+  appId: process.env.db_app_id
 };
 
 const app = initializeApp(firebaseConfig);
