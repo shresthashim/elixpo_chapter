d5f0d1be9079ccdeef8211dddbf6a27295b9eaec
Circuit-Overtime <ayushbhatt633@gmail.com>
2025-08-17 14:49:07 +0000

    updated the audio pipeline

--- torchAvailable.py
+++
@@ -1,7 +0,0 @@
-import torch
-if torch.cuda.is_available():
-    device = torch.device("cuda:0")
-    print(f"✅ Using GPU: {torch.cuda.get_device_name(device)}")
-else:
-    device = torch.device("cpu")
-    print("⚠️ CUDA not available, using CPU instead.")
