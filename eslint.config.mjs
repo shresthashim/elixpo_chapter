09c63a475f4604faf1740e709852cc2d6763ffa1
IgYaHiko <subhrokolay2@gmail.com>
2025-08-30 03:06:43 +0530

    66:try

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,12 +12,12 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    ignores: ["**/generated/*"],
-    rules: {
-      // ✅ Allow "any" globally
-      "@typescript-eslint/no-explicit-any": "off",
-    },
+     ignores: ["**/generated/*"],
+     rules: {
+       
+     }
   },
+  
 ];
 
 export default eslintConfig;
