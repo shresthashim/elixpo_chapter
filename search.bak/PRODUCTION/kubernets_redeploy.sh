8efda102b415ac86b6acbf2303d404394ae601ff
Ayushman Bhattacharya <74301576+Circuit-Overtime@users.noreply.github.com>
2025-08-30 23:43:10 +0530

    added the elixpo-search track sync

--- search.bak/PRODUCTION/kubernets_redeploy.sh
+++
@@ -1,8 +0,0 @@
-minikube stop && \
-minikube delete && \
-minikube start --driver=docker --force && \
-kubectl apply -f PRODUCTION/k8s/ --validate=false && \
-kubectl get pods && \
-minikube service elixpo-search
-
-
