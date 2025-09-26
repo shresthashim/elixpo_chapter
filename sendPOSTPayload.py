072cb8217c63eaaecd0067664c5bd8fdbad7d38f
Circuit-Overtime <ayushbhatt633@gmail.com>
2025-09-26 05:55:45 +0000

    tested audio transcriber and it works

--- sendPOSTPayload.py
+++ sendPOSTPayload.py
@@ -2,50 +2,49 @@ import requests
 import base64
 from pydub import AudioSegment
 
-# audio = AudioSegment.from_file("voice_clone.mp3", format="mp3")
-# audio.export("voice.wav", format="wav")
-# with open("voice.wav", "rb") as f:
-#     audio_bytes = f.read()
-# audio_b64 = base64.b64encode(audio_bytes).decode("utf-8")
+audio = AudioSegment.from_file("W8i19O5P6L.wav", format="wav")
+audio.export("voice.wav", format="wav")
+with open("voice.wav", "rb") as f:
+    audio_bytes = f.read()
+audio_b64 = base64.b64encode(audio_bytes).decode("utf-8")
 
 # Insert the base64 wav data into the payload for voice cloning
 
-url = "http://51.159.148.245:8000/audio"
+url = "http://51.159.143.95:8000/audio"
 payload = {
     "messages": [
         {
             "role": "system",
             "content": [
-                {"type": "text", "text": "A very scary ghastly sequel of a movie. Read the text as it is!"}
+                {"type": "text", "text": "You are an audio transcriber"}
             ]
         },
         {
             "role": "user",
             "content": [
-                {"type": "text", "text": "HAHHA! Welcome to my palace of laughter!"},
+                {"type": "text", "text": "Transcribe the users audio for me"},
                 {
                     "type": "voice",
                     "voice": {
-                        "name": "ghost",
+                        "name": "ash",
                         "format": "wav"
                     }
+                },
+                {
+                    "type": "speech_audio",
+                    "audio": {"data": f"{audio_b64}", "format": "wav"}
                 }
             ]
-            }
-            ],
-            "seed": 43
         }
-   
-
-
-
+    ],
+    "seed": 43
+}
 
 response = requests.post(url, json=payload)
 
 if response.headers.get("Content-Type") == "audio/wav":
     with open("output2.wav", "wb") as f:
         f.write(response.content)
-    print("Audio saved as output.wav")
-
+    print("Audio saved as output2.wav")
 else:
     print(response.json())
