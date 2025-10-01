93beb73478ebaba2c23220f012430d6f725b1a58
Ayushman Bhattacharya <74301576+Circuit-Overtime@users.noreply.github.com>
2025-10-01 19:29:06 +0530

    removed uncessary comments (dummy commit)

--- art.elixpo/JS/auth/login_with_gitHub.js
+++ art.elixpo/JS/auth/login_with_gitHub.js
@@ -9,8 +9,8 @@ document.getElementById("signin_with_github").addEventListener("click", () => {
 
         var docRef = db.collection("users").doc(user.displayName);
         var today  = new Date();
-        var date = today.getDate() + "/" + (today.getMonth()+1) + "/" + today.getFullYear() ; //gives the  current date to the system
-        docRef.get().then((doc) => { // gets the whole data against the entered email address
+        var date = today.getDate() + "/" + (today.getMonth()+1) + "/" + today.getFullYear() ; 
+        docRef.get().then((doc) => { 
             if (doc.exists) {
                 tileFlash();
                 if(doc.data().isDev == "DEV")
